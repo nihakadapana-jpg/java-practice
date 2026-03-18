8ba6a72d611145a1fba48aafaea1fe1ce92eb59b
nihakadapana <nihakadapana@gmail.com>
2026-03-18 10:50:09 +0530

    Add WordCount class to count words in a string

---
+++ WordCount.java
@@ -0,0 +1,7 @@
+class WordCount{
+    public static void main(String[] args){
+        String s = "I love Java";
+        String words[] = s.split(" ");
+        System.out.println("Number of words: " + words.length);
+    }
+}
