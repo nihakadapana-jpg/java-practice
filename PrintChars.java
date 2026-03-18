0d6954f29a6ea292029f71be8a955609a2baa21f
nihakadapana <nihakadapana@gmail.com>
2026-03-18 10:54:25 +0530

    Add PrintChars class to print characters of a string

---
+++ PrintChars.java
@@ -0,0 +1,9 @@
+class PrintChars{
+    public static void main(String[] args){
+        String s="niha";
+
+        for(char c : s.toCharArray()){
+            System.out.println(c);
+        }
+    }
+}
