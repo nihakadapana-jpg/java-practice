bda3ba1f792ed974aa599aa99f68493f2cfba74f
nihakadapana <nihakadapana@gmail.com>
2026-03-18 10:32:25 +0530

    Add Reverse class to reverse a string

---
+++ Reverse.java
@@ -0,0 +1,7 @@
+class Reverse {
+    public static void main(String[] args) {
+        String s = "niha";
+        String rev = new StringBuilder(s).reverse().toString();
+        System.out.println(rev);
+    }
+}
